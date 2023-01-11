1cdae990647f41b3d63fed97db96a47442321242
JardielN <jardielnruiz@gmail.com>
2023-01-10 19:16:41 -0600

    First Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\Contact;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        $this->call([
+            CompaniesTableSeeder::class,
+            ContactsTableSeeder::class,
+        ]);
+        */
+
+        Company::factory()->count(10)->create();
+        Contact::factory()->count(50)->create();
+    }
+}
